5ba9f7e9eccf69cfd272c106abf4a0afce369ce3
vacherkasskiy <90340909+vacherkasskiy@users.noreply.github.com>
2021-09-26 00:37:01 +0300

    Add files via upload

---
+++ Module1/Seminar_HW/Task_6.01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_6._01
+{
+    class Program
+    {
+        static public void Compression(ref int[] arr, out int n)
+        {
+            n = 0;
+
+            for (int i = 0; i < arr.Length - 1 - n; ++i)
+            {
+                if ((arr[i] + arr[i + 1]) % 3 == 0)
+                {
+                    arr[i] *= arr[i + 1];
+                    n += 1;
+
+                    for (int k = i + 1; k < arr.Length - 1; ++k)
+                    {
+                        arr[k] = arr[k + 1];
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            Compression(ref arr, out n);
+            for (int i = 0; i < arr.Length - n; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ Module1/Seminar_HW/Task_6.02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_6._02
+{
+    class Program
+
+    {
+        static public void Compression(ref int[] arr, out int n)
+        {
+            n = 0;
+
+            for (int i = 0; i < arr.Length - n; ++i)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    ++n;
+                    --i;
+
+                    for (int k = i + 1; k < arr.Length - 1; ++k)
+                    {
+                        arr[k] = arr[k + 1];
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int n, amount = int.Parse(Console.ReadLine());
+            int[] arr = new int[amount];
+            
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = rand.Next(-10, 10); 
+            }
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Compression(ref arr, out n);
+            Console.WriteLine("");
+            for (int i = 0; i < arr.Length - n; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
