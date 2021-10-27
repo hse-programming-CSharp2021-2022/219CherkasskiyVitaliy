509b660a194aa2187b610e728225ab5b1cf3d94b
vacherkasskiy <90340909+vacherkasskiy@users.noreply.github.com>
2021-10-27 21:28:36 +0300

    Add files via upload

---
+++ Module_2/HW_1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+public partial class Task_2
+{
+    class Point
+    {
+        double X { get; set; }
+        double Y { get; set; }
+        public Point(double x = 0, double y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+        public double R
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+            }
+        }
+        double F
+        {
+            get
+            {
+                if (X > 0 && Y >= 0)
+                {
+                    return (Math.Atan(Y / X));
+                }
+                if (X > 0 && Y < 0)
+                {
+                    return (Math.Atan(Y / X) + Math.PI * 2);
+                }
+                if (X < 0)
+                {
+                    return (Math.Atan(Y / X) + Math.PI);
+                }
+                if (X == 0 && Y > 0)
+                {
+                    return (Math.PI / 2);
+                }
+                if (X == 0 && Y < 0)
+                {
+                    return (Math.PI * 3 / 2);
+                }
+                return 0;
+            }
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Декартовы координаты --> X = {X} ; Y = {Y}");
+            Console.WriteLine($"Полярные координаты  --> R = {R} ; F = {F}");
+        }
+    }
+    public static void Main()
+    {
+        Random rand = new Random();
+        Point point1, point2, point3;
+        double x, y;
+        // ------------------------------ //
+        while (true)
+        {
+            point1 = new Point(rand.Next(-100, 100), rand.Next(-100, 100));
+            point2 = new Point(rand.Next(-100, 100), rand.Next(-100, 100));
+            Console.Write("Введите значение координаты Х для третьей точки: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Введите значение координаты Y для третьей точки: ");
+            y = double.Parse(Console.ReadLine());
+            if (x == 0 && y == 0)
+            {
+                break;
+            }
+            point3 = new Point(x, y);
+            Console.WriteLine("------------");
+
+            Point[] listOfPoints = { point1, point2, point3 };
+            var sortedList = from value in listOfPoints
+                             orderby (value.R)
+                             select value;
+            foreach (var item in sortedList)
+            {
+                item.PrintData();
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------");
+        }
+        // ------------------------------ //
+        Console.WriteLine("Конец :)");
+    }
+}
---
+++ Module_2/HW_1/ConsoleApp2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+public partial class Task_1
+{
+    class RegularPolygon
+    {
+        int amountOfSides { get; set; }
+        double circleRadius { get; set; }
+        public RegularPolygon(int x = 0, double y = 0)
+        {
+            amountOfSides = x;
+            circleRadius = y;
+        }
+        public double Perimeter()
+        {
+            return 2 * circleRadius * Math.Tan(Math.PI / 2 / amountOfSides);
+        }
+        public double Square()
+        {
+            return amountOfSides * Math.Pow(circleRadius, 2) * Math.Tan(Math.PI / 2 / amountOfSides);
+        }
+        public string PolygonData()
+        {
+            return $"Perimetr is equal to: {this.Perimeter()}\n" +
+                   $"Square is equal to: {this.Square()}"; 
+        }
+    }
+    public static void Main()
+    {
+        Console.Write("Enter amount of objects: ");
+        int amountOfObjects = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("-----------");
+        Console.WriteLine();
+        RegularPolygon[] objects = new RegularPolygon[amountOfObjects];
+        int tempAmount;
+        double tempRadius;
+        for (int i = 0; i < objects.Length; ++i)
+        {
+            Console.Write($"Enter amount of sides for {i+1} object: ");
+            tempAmount = int.Parse(Console.ReadLine());
+            Console.Write($"Enter radius of inner circle for {i+1} object: ");
+            tempRadius = int.Parse(Console.ReadLine());
+            // Initialize values.
+            objects[i] = new RegularPolygon(tempAmount, tempRadius);
+            Console.WriteLine();
+            Console.WriteLine("-----------");
+            Console.WriteLine();
+        }
+        double minPerimetr = objects[0].Perimeter(), maxPerimetr = objects[0].Perimeter(), 
+               minSquare = objects[0].Square(), maxSquare = objects[0].Square();
+        for (int i = 0; i < objects.Length; ++i)
+        {
+            // Get max & min values.
+            if (i > 0)
+            {
+                if (minPerimetr > objects[i].Perimeter())
+                {
+                    minPerimetr = objects[i].Perimeter();
+                }
+                if (maxPerimetr < objects[i].Perimeter())
+                {
+                    maxPerimetr = objects[i].Perimeter();
+                }
+                if (minSquare > objects[i].Square())
+                {
+                    minSquare = objects[i].Square();
+                }
+                if (maxSquare < objects[i].Square())
+                {
+                    maxSquare = objects[i].Square();
+                }
+            }
+        }
+        Console.Write("Minimal perimetr is equal to ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(minPerimetr);
+        Console.WriteLine();
+        //------------------------------------------//
+        Console.ResetColor();
+        Console.Write("Minimal square is equal to ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(minSquare);
+        Console.WriteLine();
+        //------------------------------------------//
+        Console.ResetColor();
+        Console.Write("Maximal perimetr is equal to ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(maxPerimetr);
+        Console.WriteLine();
+        //------------------------------------------//
+        Console.ResetColor();
+        Console.Write("Maximal square is equal to ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(maxSquare);
+        Console.ResetColor();
+    }
+}
