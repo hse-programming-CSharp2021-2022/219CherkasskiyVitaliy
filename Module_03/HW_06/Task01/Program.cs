341459146156b1e8ba06e3073b15e6b182a99d31
vacherkasskiy <90340909+vacherkasskiy@users.noreply.github.com>
2022-03-26 00:53:23 +0300

    Add files via upload

---
+++ Module_03/HW_06/Task01/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Task01
+{
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Point(double x = 0, double y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+        public double Distance(Point B)
+        {
+            return Math.Sqrt((B.X - X) * (B.X - X) + (B.Y - Y) * (B.Y - Y));
+        }
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+    }
+    class Circle: IComparable<Circle>
+    {
+        Point O;
+        double rad;
+        public Circle(double x, double y, double r)
+        {
+            this.O = new Point(x, y);
+            rad = r;
+        }
+        public override string ToString()
+        {
+            return $"радиус: {rad}, центр: {O}";
+        }
+
+        public int CompareTo(Circle other)
+        {
+            if (this < other)
+                return 1;
+            return 1;
+        }
+
+        public static bool operator >(Circle W1, Circle W2)
+        {
+            return W1.O.Distance(new Point()) * W1.rad > W2.O.Distance(new Point()) * W2.rad;
+        }
+        public static bool operator <(Circle W1, Circle W2)
+        {
+            return W1.O.Distance(new Point()) * W1.rad < W2.O.Distance(new Point()) * W2.rad;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var circles1 = new List<Circle>();
+            var circles2 = new List<Circle>();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                circles1.Add(new Circle(rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10)));
+                circles2.Add(new Circle(rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10)));
+            }
+
+            circles1.Sort();
+            circles2.Sort();
+
+            circles1.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("--------");
+            circles2.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ Module_03/HW_07/Task01/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task01
+{
+    struct Person : IComparable<Person>
+    {
+        string _name;
+        string _lastname;
+        int _age;
+
+        public int Age => _age;
+        public string Name => _name;
+        public string Lastname => _lastname;
+        public Person(string name, string lastname, int age)
+        {
+            if (name.Length == 0 || lastname.Length == 0 || age <= 0)
+            {
+                throw new ArgumentException("Incorrect input");
+            }
+
+            _name = name;
+            _lastname = lastname;
+            _age = age;
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (Age < other.Age)
+            {
+                return -1;
+            }
+            return 1;
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Lastname}, age = {Age}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var characters = new List<Person>();
+            var rnd = new Random();
+
+            for (int i = 0; i < n; ++i)
+            {
+                string name = "";
+                string lastName = "";
+
+                for (int j = 0; j < rnd.Next(0,8); ++j)
+                {
+                    name += (char)rnd.Next('a', 'z');
+                }
+                for (int j = 0; j < rnd.Next(0, 8); ++j)
+                {
+                    lastName += (char)rnd.Next('a', 'z');
+                }
+
+                try
+                {
+                    characters.Add(new Person(name, lastName, rnd.Next(-5, 90)));
+                }
+                catch (ArgumentException e)
+                {
+                    --i;
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+
+            characters.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("--sorted below--");
+            characters.Sort();
+            characters.ForEach(x => Console.WriteLine());
+        }
+    }
+}
