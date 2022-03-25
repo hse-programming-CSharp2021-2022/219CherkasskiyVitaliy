07584d0722022548baa1af8dee431a75d0b8a8d4
vacherkasskiy <90340909+vacherkasskiy@users.noreply.github.com>
2022-03-26 00:49:31 +0300

    Add files via upload

---
+++ Module_03/HW_01/Task01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        delegate int Cast(double a);
+        static void Main(string[] args)
+        {
+            Cast cast1 = delegate (double a)
+            {
+                if ((int)a % 2 == 0)
+                {
+                    return (int)a;
+                }
+
+                return (int)a + 1;
+            };
+
+            Cast cast2 = delegate (double a)
+            {
+                return ((int)a).ToString().Length;
+            };
+
+            Console.WriteLine(cast1(12312.6));
+            Console.WriteLine(cast2(38848.888));
+        }
+    }
+}
---
+++ Module_03/HW_01/Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task02
+{
+    delegate double NewDelegate(double x);
+    class TemperatureConventerImp
+    {
+        public double Celcium2Faringate(double x)
+        {
+            return ((double)5 / 9) * (x - 32);
+        }
+        public double Faringate2Celcium (double x)
+        {
+            return ((double)9 / 5) * x + 32;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            NewDelegate delegate1 = new NewDelegate(new TemperatureConventerImp().Celcium2Faringate);
+            delegate1 += new TemperatureConventerImp().Faringate2Celcium;
+            Console.WriteLine(delegate1(18));
+        }
+    }
+}
