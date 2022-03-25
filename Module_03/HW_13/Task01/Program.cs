2ff2cd0bc15769c9c23260b93fc668c2e14206a5
vacherkasskiy <90340909+vacherkasskiy@users.noreply.github.com>
2022-03-26 00:56:32 +0300

    Add files via upload

---
+++ Module_03/HW_13/Task01/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+namespace Task01
+{
+    class Fibbonacci : IEnumerable
+    {
+        int f0 = 1, f1 = 1;
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                yield return f0;
+                int c = f0;
+                f0 = f1;
+                f1 += c;
+            }
+        }
+        public IEnumerable NameEnumerator(int n)
+        {
+            int f0 = 1, f1 = 1;
+            for (int i = 0; i < n; ++i)
+            {
+                yield return f0;
+                int c = f0;
+                f0 = f1;
+                f1 += c;
+            }
+        }
+    }
+    class TriangleNums : IEnumerable
+    {
+        int num = 1;
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                yield return num;
+                num += (i + 2);
+            }
+        }
+        public IEnumerable NameEnumerator(int n)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                yield return num;
+                num += (i + 2);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacci = new Fibbonacci();
+            var triangleNums = new TriangleNums();
+            var array = new int[10];
+
+            var counter = 0;
+            foreach (object el in fibonacci.NameEnumerator(10))
+            {
+                Console.WriteLine(el);
+                array[counter] = (int)el;
+                ++counter;
+            }
+            counter = 0;
+            foreach (object el in triangleNums.NameEnumerator(10))
+            {
+                Console.WriteLine(el);
+                array[counter] += (int)el;
+                ++counter;
+            }
+            foreach (int el in array)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
---
+++ Module_03/HW_14/Task01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var array = new int[n];
+            Random random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-1000, 1001);
+            }
+            foreach (var el in array)
+            {
+                Console.WriteLine(el);
+            }
+
+            Console.WriteLine();
+
+            var newArray = array.Select(x => x * x).ToArray();
+            Array.ForEach(newArray, Console.WriteLine);
+            Console.WriteLine("==========");
+
+            newArray = array.Where(x => x > 0 && x < 100).ToArray();
+            Array.ForEach(newArray, Console.WriteLine);
+            Console.WriteLine("==========");
+
+            newArray = array.Where(x => x % 2 == 0).OrderByDescending(x => x).ToArray();
+            Array.ForEach(newArray, Console.WriteLine);
+            Console.WriteLine("==========");
+
+            var newArray2 = array.GroupBy(x => x.ToString().Length).ToArray();
+            Array.ForEach(newArray2, x =>
+            {
+                Array.ForEach(x.ToArray(), Console.WriteLine);
+            });
+        }
+    }
+}
